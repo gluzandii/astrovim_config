869676f9e562e1d9e66b41f41abefeb411a6c2bc
Sushant Nariangadu <nsushant2007@gmail.com>
2023-05-16 16:39:02 +0530

    Add nim support.

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,34 +1,35 @@
 -- customize mason plugins
 return {
-  -- use mason-lspconfig to configure LSP installations
-  {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "tsserver",
-        "eslint",
-        "rust_analyzer",
-        "taplo",
-        "svelte",
-        "gopls",
-      },
+    -- use mason-lspconfig to configure LSP installations
+    {
+        "williamboman/mason-lspconfig.nvim",
+        -- overrides `require("mason-lspconfig").setup(...)`
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "tsserver",
+                "eslint",
+                "rust_analyzer",
+                "taplo",
+                "svelte",
+                "gopls",
+                "cssls",
+            },
+        },
     },
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = {
-      ensure_installed = { "prettier", "stylua", "yamlfmt", "rustfmt", "gofumpt", "goimports" },
+    -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+    {
+        "jay-babu/mason-null-ls.nvim",
+        -- overrides `require("mason-null-ls").setup(...)`
+        opts = {
+            ensure_installed = { "prettier", "stylua", "yamlfmt", "rustfmt", "gofumpt", "goimports" },
+        },
     },
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = {
-      -- ensure_installed = { "rust" },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        -- overrides `require("mason-nvim-dap").setup(...)`
+        opts = {
+            -- ensure_installed = { "rust" },
+        },
     },
-  },
 }
